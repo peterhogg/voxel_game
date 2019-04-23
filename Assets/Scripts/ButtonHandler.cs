e69399745a6322b75435e9c4648257501c7de343
Peter Hoggarth <hoggarth.peter@gmail.com>
2019-04-22 23:15:07 -0400

    Added menu

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonHandler : MonoBehaviour {
+	//Make sure to attach these Buttons in the Inspector
+	public Button randomButton;
+	public Button emptyButton;
+
+	void Start() {
+		emptyButton.onClick.AddListener(loadEmpty);
+		randomButton.onClick.AddListener(loadRandom);
+	}
+
+	void loadEmpty() {
+		Application.LoadLevel("Empty");
+	}
+
+	void loadRandom() {
+		Application.LoadLevel("Random");
+	}
+}
---
+++ Assets/Scripts/EmptySpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EmptySpawner : MonoBehaviour {
+	public GameObject terrain;
+	public GameObject baseBlock;
+	public int x;
+	public int y;
+	public int z;
+	// Use this for initialization
+	void Start() {
+		Dictionary<int, GameObject> blockMap = new Dictionary<int, GameObject>();
+		blockMap.Add(1, baseBlock);
+		blockMap.Add(2, terrain);
+
+		int[,,] map = new int[x, z, y];
+
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				for (int k = 0; k < z; k++) {
+					if (k == 0) {
+						map[i, k, j] = 1;
+						GameObject spawn2 = Instantiate<GameObject>(baseBlock);
+						spawn2.transform.localPosition = new Vector3(i, -z + k, j);
+					} 
+				}
+
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
